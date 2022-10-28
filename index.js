dfd0f1b11a62449ef3d7bc4f45380aadca937a18
Justin Rinehuls <jrinehuls@gmail.com>
2022-10-28 19:50:59 -0400

    Rework program as a web-based GUI application.

--- index.js
+++ index.js
@@ -1,66 +1,79 @@
-let compIndex
-let compChoice
-let playerIndex
-let playerChoice
-let winner
-let playerScore = 0
-let compScore = 0
+let compIndex;
+let compChoice;
+let playerIndex;
+let playerChoice;
+let winner;
+let playerScore = 0;
+let compScore = 0;
 
-let choices = ["rock", "paper", "scissors"]
+let choices = ["ROCK", "PAPER", "SCISSORS"];
+const buttons = document.querySelectorAll('button');
 
 function getComputerChoice() {
-    compIndex = Math.floor(Math.random()*3)
-    return choices[compIndex]
+    compIndex = Math.floor(Math.random()*3);
+    return choices[compIndex];
 }
 
-function displayWinner(playerChoice, compChoice) {
-    switch(playerChoice + "|" + compChoice) {
+function roundWinner(playerChoice, compChoice) {
+    switch(playerChoice.toLowerCase() + "|" + compChoice.toLowerCase()) {
         case "rock|paper":
             compScore+=1
-            return "Computer wins";
+            return "Computer wins this round.";
         case "rock|scissors":
             playerScore+=1
-            return "Player wins"
+            return "Player wins this round."
         case "scissors|rock":
             compScore+=1
-            return "Computer wins"
+            return "Computer wins this round."
         case "scissors|paper":
             playerScore+=1
-            return "Player wins"
+            return "Player wins this round."
         case "paper|scissors":
             compScore+=1
-            return "Computer wins";
+            return "Computer wins this round.";
         case "paper|rock":
             playerScore+=1
-            return "Player wins"
+            return "Player wins this round."
         default:
-            return "Tie";
+            return "This round resulted in a tie.";
     }
 }
 
-function game() {
-    for (let i = 1; i <= 5; i++) {
-        playerIndex = prompt("Type the integer of your choice\n 1. rock\n2. paper\n3. scissors")
-        playerChoice = choices[playerIndex-1]
-        compChoice = getComputerChoice()
-        winner = displayWinner(playerChoice, compChoice)
-        
-        console.log("Player chose: " + playerChoice)
-        console.log("Computer chose:", compChoice)
-        console.log(winner)
-    }
-    console.log("Player Score: " + playerScore)
-    console.log("Computer score: " + compScore)
-
-    if (playerScore > compScore) {
-        console.log("Player is the winner!")
-    }
-    else if (playerScore < compScore) {
-        console.log("Computer is the winner!")       
-    }
-    else {
-        console.log("There is no winner!")
+function gameOver() {
+    winner = document.getElementById("winner");
+    if (playerScore == 5 || compScore == 5) {
+        disableButtons();
+        if (playerScore > compScore) {
+            winner.innerHTML = "Player is the winner!";
+        }
+        else if (playerScore < compScore) {
+            winner.innerHTML = "Computer is the winner!";    
+        }
     }
 }
 
-game()
+function disableButtons() {
+    buttons.forEach(button => { button.disabled = true })
+}
+
+function playRound(selection) {
+    playerChoice = selection;
+    compChoice = getComputerChoice();
+    winner = roundWinner(playerChoice, compChoice);
+
+    document.getElementById('roundWinner').innerHTML = `
+    Player chose: ${playerChoice}<br>
+    Computer chose: ${compChoice}<br>
+    ${winner}
+    `;
+
+    document.getElementById('playerScore').innerHTML = playerScore;
+    document.getElementById('compScore').innerHTML = compScore;
+    gameOver();
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        playRound(button.value)
+    })
+});
